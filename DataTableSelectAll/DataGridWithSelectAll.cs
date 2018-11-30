36eaf20301fad60ebaeb2fb8544becaec48e7801
fuqunaga <fukhid@gmail.com>
2018-11-30 19:25:31 +0900

    first commit

---
+++ DataTableSelectAll/DataGridWithSelectAll.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DataTableSelectAll
+{
+    class DataGridWithSelectAll: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DataTable Items { get; set; }
+
+
+        public DataGridWithSelectAll(DataGrid dataGrid)
+        {
+            Items = CreateItems();
+
+            dataGrid.AutoGeneratingColumn += (s, e) =>
+            {
+                if (e.PropertyName == "IsSelected")
+                {
+                    var c = new DataGridTemplateColumn()
+                    {
+                        CellTemplate = (DataTemplate)dataGrid.Resources["IsSelected"],
+                        Header = e.Column.Header,
+                        HeaderTemplate = (DataTemplate)dataGrid.Resources["SelectAll"],
+                        HeaderStringFormat = e.Column.HeaderStringFormat,
+                        SortMemberPath = e.PropertyName // this is used to index into the DataRowView so it MUST be the property's name (for this implementation anyways)
+                    };
+                    e.Column = c;
+
+                }
+            };
+
+            dataGrid.DataContext = this;
+        }
+
+        private DataTable CreateItems()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("IsSelected", typeof(bool));
+            dt.Columns.Add("StringColumn", typeof(string));
+            dt.Columns.Add("IntColumn", typeof(int));
+
+            dt.Rows.Add(true, "row0", 0);
+            dt.Rows.Add(false, "row1", 1);
+            dt.Rows.Add(true, "row2", 2);
+
+            dt.ColumnChanged += (s, e) =>
+            {
+                if (e.Column.ColumnName == "IsSelected")
+                {
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsSelected"));
+                }
+            };
+
+            return dt;
+        }
+
+
+
+        public bool? IsSelected
+        {
+            get
+            {
+                var uniqueList = Items.AsEnumerable().Select(row => row["IsSelected"]).Distinct().ToList();
+                return uniqueList.Count() == 1 ? (bool?)uniqueList.First() : null;
+            }
+            set
+            {
+                Items.AsEnumerable().ToList().ForEach(row => row["IsSelected"] = value);
+            }
+        }
+    }
+}
---
+++ DataTableSelectAll/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataTableSelectAll
+{
+    /// <summary>
+    /// DataTableにBindingしつつSelectAllチェックボックスをヘッダーにつけるDataGridのサンプル
+    /// https://stackoverflow.com/questions/25643765/wpf-datagrid-databind-to-datatable-cell-in-celltemplates-datatemplate
+    /// https://stackoverflow.com/questions/21356662/selectall-in-datagrid
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataGridWithSelectAll dataGridWithSelectAll;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dataGridWithSelectAll = new DataGridWithSelectAll(MyDataGrid);
+        }
+
+
+/*
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(string.Join(",", Items.AsEnumerable().Select(row => row["IsSelected"].ToString())));
+            Debug.WriteLine(string.Join(",", Items.AsEnumerable().Select(row => row["StringColumn"].ToString())));
+        }
+        */
+    }
+}
