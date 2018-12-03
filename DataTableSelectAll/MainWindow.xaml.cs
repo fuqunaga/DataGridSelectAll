ec1895fa90031b0acbf591d7f92cd89446b49ae9
fuqunaga <fukhid@gmail.com>
2018-12-03 15:00:02 +0900

    add sample code to project add gif

--- DataTableSelectAll/MainWindow.xaml.cs
+++ DataTableSelectAll/MainWindow.xaml.cs
@@ -1,20 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace DataTableSelectAll
 {
@@ -25,22 +9,12 @@ namespace DataTableSelectAll
     /// </summary>
     public partial class MainWindow : Window
     {
-        DataGridWithSelectAll dataGridWithSelectAll;
-
         public MainWindow()
         {
             InitializeComponent();
 
-            dataGridWithSelectAll = new DataGridWithSelectAll(MyDataGrid);
+            var dataGridWithSelectAll = new DataGridWithSelectAllSample();
+            dataGridWithSelectAll.Bind(MyDataGrid);
         }
-
-
-/*
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Debug.WriteLine(string.Join(",", Items.AsEnumerable().Select(row => row["IsSelected"].ToString())));
-            Debug.WriteLine(string.Join(",", Items.AsEnumerable().Select(row => row["StringColumn"].ToString())));
-        }
-        */
     }
 }
